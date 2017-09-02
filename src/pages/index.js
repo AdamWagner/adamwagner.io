734d682a88a1fd6a239374ab9e0dd7226fb5dffd
Adam Wagner <awagnerjcu08@gmail.com>
2017-09-01 17:06:56 -0700

    restting gh-pages branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Here is an example paragraph.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
